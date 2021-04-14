08dba9833ce41380c7eab7af3cf8f12873d9744d
alineolic <72208027+alineolic@users.noreply.github.com>
2021-04-14 21:13:36 +0100

    Added links to GitHub repo and Netlify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
 
         <p>
-          This project was coded by <a href="https://www.alineclaudio.com" target="_blank"> Aline Claudio</a> and is open-sourced on <a href="https://github.com/alineolic/new-react-weather-app" target="_blank">GitHub</a> and hosted on <a href="https://app.netlify.com/" target="_blank"></a>Netlify.
+          This project was coded by <a href="https://www.alineclaudio.com" target="_blank"> Aline Claudio</a> and is open-sourced on <a href="https://github.com/alineolic/new-react-weather-app" target="_blank">GitHub</a> and hosted on <a href="https://app.netlify.com/" target="_blank"></a>Netlify</a>.
         </p>
       </header>
     </div>
