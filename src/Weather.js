05c2ed0a9f977ede0ba99161cb0dae74e7854a97
alineolic <72208027+alineolic@users.noreply.github.com>
2021-04-18 19:48:29 +0100

    Added search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
 import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
-import "./index.css";
+
 
 export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
 const [weatherData, setWeatherData] = useState({ready: false});
 function handleResponse(response) {
   setWeatherData({
@@ -20,13 +20,29 @@ function handleResponse(response) {
   });
 }
 
+function search() {
+
+  const apiKey = "3a5b83d2290ca8aeea736a82a10e7ea7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  search();
+}
+
+function handleCityChange(event){
+setCity(event.target.value);
+}
+
 if (weatherData.ready) {
   return (
         <div className="Weather">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="row">
               <div className="col-9">
-            <input type="search" placeholder="Search city..." className="form-control" autoFocus="on"/>
+            <input type="search" placeholder="Search city..." className="form-control" autoFocus="on" onChange={handleCityChange} />
             </div>
               <div className="col-3">
             <input type="submit" value="Search" className="btn btn-primary w-100" />
@@ -37,10 +53,7 @@ if (weatherData.ready) {
             </div>
     )
 } else {
-  const apiKey = "3a5b83d2290ca8aeea736a82a10e7ea7";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  
-  return "Loading..."
+  searc();
+    return "Loading..."
 }
 }
