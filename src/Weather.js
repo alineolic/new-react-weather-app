eaf1184764f7c80a04f8e005c1736860816a2d85
alineolic <72208027+alineolic@users.noreply.github.com>
2021-04-18 19:25:26 +0100

    Created a Weather Info component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate"
+import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 import "./index.css";
@@ -32,36 +33,7 @@ if (weatherData.ready) {
             </div>
             </div>
           </form>
-           <h1>{weatherData.city}</h1>
-           <ul>
-               <li>
-                 <FormattedDate date={weatherData.date} />
-               </li>
-               <li className="text-capitalize">
-                   {weatherData.description}
-               </li>
-           </ul>
-           <div className="row mt-3">
-             <div className="col-6">
-               <div className="clearfix">
-               <img src={weatherData.iconUrl} alt={weatherData.description} className="float-left" />
-               <div className="float-left">
-               <span className="temperature">{Math.round(weatherData.temperature)}</span>
-               <span className="unit">˚C</span>
-               </div>
-               </div>
-           </div>
-           <div className="col-6">
-             <ul>
-               <li>
-                 Humidity: {weatherData.humidity} %
-               </li>
-               <li>
-                 Wind: {Math.round(weatherData.wind)} km/H
-               </li>
-             </ul>
-           </div>
-           </div>
+          <WeatherInfo data={weatherData} />
             </div>
     )
 } else {
---
+++ src/WeatherInfo.js
@@ -0,0 +1,38 @@
+import React from "./react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+return (
+<div className="WeatherInfo"> 
+<h1>{weatherData.city}</h1>
+           <ul>
+               <li>
+                 <FormattedDate date={props.data.date} />
+               </li>
+               <li className="text-capitalize">
+                   {weatherData.description}
+               </li>
+           </ul>
+           <div className="row mt-3">
+             <div className="col-6">
+               <div className="clearfix">
+               <img src={weatherData.iconUrl} alt={weatherData.description} className="float-left" />
+               <div className="float-left">
+               <span className="temperature">{Math.round(weatherData.temperature)}</span>
+               <span className="unit">˚C</span>
+               </div>
+               </div>
+           </div>
+           <div className="col-6">
+             <ul>
+               <li>
+                 Humidity: {weatherData.humidity} %
+               </li>
+               <li>
+                 Wind: {Math.round(weatherData.wind)} km/H
+               </li>
+             </ul>
+           </div>
+           </div>
+           </div>);
+           }
